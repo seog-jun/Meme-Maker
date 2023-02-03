e221ee96da12685f5e6b83a7cac3ea46f116e2a4
seog-jun <shong69@myseneca.ca>
2023-02-03 12:20:32 -0500

    Drawing line with click

--- app.js
+++ app.js
@@ -4,15 +4,11 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
 
-ctx.fillRect(210 - 40, 200 - 20, 15, 100);
-ctx.fillRect(350 - 40, 200 - 20, 15, 100);
-ctx.fillRect(260 - 40, 200 - 20, 60, 200);
+ctx.lineWidth = 2;
 
-ctx.arc(250, 100, 50, 0, 2 * Math.PI);
-ctx.fill();
+function onClick(event) {
+  ctx.lineTo(event.offsetX, event.offsetY);
+  ctx.stroke();
+}
 
-ctx.beginPath();
-ctx.fillStyle = "white";
-ctx.arc(260 + 10, 80, 8, Math.PI, 2 * Math.PI);
-ctx.arc(220 + 10, 80, 8, Math.PI, 2 * Math.PI);
-ctx.fill();
+canvas.addEventListener("click", onClick);
